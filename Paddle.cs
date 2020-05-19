afc0b357c1c0ae10d53651c98ec223b23892055a
kevinindrian <kevintvakayil@gmail.com>
2020-05-19 18:02:39 +0530

    blockbreaker initial phase

---
+++ Paddle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    //config para
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float ScreenWidth = 16f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePos = Input.mousePosition.x / Screen.width * ScreenWidth;
+        Vector2 padPos = new Vector2(transform.position.x, transform.position.y);
+        padPos.x = Mathf.Clamp(mousePos, minX, maxX);
+        transform.position = padPos;
+
+
+
+    }
+
+}
